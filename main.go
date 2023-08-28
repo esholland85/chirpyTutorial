88187177c13fa15885800cc74bd18f113edb2c96
esholland85 <esholland85@gmail.com>
2023-08-28 14:40:46 -0700

    add query handling

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -33,6 +34,7 @@ func main() {
 	godotenv.Load()
 	apiCfg := &apiConfig{}
 	apiCfg.secret = os.Getenv("JWT_SECRET")
+	apiCfg.polkaKey = os.Getenv("POLKA_SECRET")
 
 	directory := http.Dir(".")
 	fsHandler := http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(directory)))
@@ -60,10 +62,14 @@ func main() {
 	r.Handle("/app/*", fsHandler)
 	r.Handle("/app", fsHandler)
 	apiR.Get("/healthz", healthz)
-	apiR.Post("/chirps", myDB.ValidationHandler)
+	apiR.Post("/chirps", func(w http.ResponseWriter, r *http.Request) { myDB.PostHandler(w, r, apiCfg) })
 	apiR.Get("/chirps", myDB.GetChirps)
 	apiR.Get("/chirps/*", myDB.GetChirp)
+	apiR.Delete("/chirps/*", func(w http.ResponseWriter, r *http.Request) { myDB.DeleteHandler(w, r, apiCfg) })
 	apiR.Post("/login", func(w http.ResponseWriter, r *http.Request) { LoginHandler(w, r, apiCfg, myDB) })
+	apiR.Post("/polka/webhooks", func(w http.ResponseWriter, r *http.Request) { myDB.UpgradeUser(w, r, apiCfg) })
+	apiR.Post("/refresh", func(w http.ResponseWriter, r *http.Request) { myDB.RefreshToken(w, r, apiCfg) })
+	apiR.Post("/revoke", func(w http.ResponseWriter, r *http.Request) { myDB.RevokeToken(w, r, apiCfg) })
 	apiR.Post("/users", myDB.UserHandler)
 	apiR.Put("/users", func(w http.ResponseWriter, r *http.Request) { myDB.UpdateUser(w, r, apiCfg) })
 	adminR.Get("/metrics", func(w http.ResponseWriter, r *http.Request) { hitzHandler(w, r, apiCfg) })
@@ -84,22 +90,32 @@ type responseRecorder struct {
 type apiConfig struct {
 	fileserverHits int
 	secret         string
+	polkaKey       string
 }
 
 type dbStructure struct {
-	Chirps map[int]Chirp `json:"chirps"`
-	Users  map[int]User  `json:"users"`
+	Chirps map[int]Chirp   `json:"chirps"`
+	Users  map[int]User    `json:"users"`
+	Auths  map[string]Auth `json:"auths"`
 }
 
 type Chirp struct {
-	Body string `json:"body"`
-	ID   int    `json:"id"`
+	Body      string `json:"body"`
+	ID        int    `json:"id"`
+	Author_ID int    `json:"author_id"`
 }
 
 type User struct {
-	Email    string `json:"email"`
-	ID       int    `json:"id"`
-	Password string `json:"password"`
+	Email         string `json:"email"`
+	ID            int    `json:"id"`
+	Password      string `json:"password"`
+	Is_Chirpy_Red bool   `json:"is_chirpy_red"`
+}
+
+type Auth struct {
+	Token       string    `json:"token"`
+	Revoked     bool      `json:"revoked"`
+	TimeRevoked time.Time `json:"timerevoked"`
 }
 
 func healthz(w http.ResponseWriter, req *http.Request) {
@@ -163,18 +179,28 @@ func (r *responseRecorder) WriteHeader(status int) {
 	fmt.Println("responserecorder... setup?")
 }
 
-func (db *DB) ValidationHandler(w http.ResponseWriter, r *http.Request) {
-	oldChirps, err1 := db.LoadDB()
+func (db *DB) PostHandler(w http.ResponseWriter, r *http.Request, cfg *apiConfig) {
+	userID, ok := db.AuthenticateUser(w, r, cfg)
+	if !ok {
+		return
+	}
+
+	wholeDB, err1 := db.LoadDB()
 	if err1 != nil {
 		respondWithError(w, http.StatusInternalServerError, "Server experienced an error")
 		return
 	}
-	chirpCount := len(oldChirps.Chirps)
+	chirpCount := len(wholeDB.Chirps)
+
+	type incChirp struct {
+		Body string `json:"body"`
+	}
 
 	decoder := json.NewDecoder(r.Body)
-	params := Chirp{}
+	params := incChirp{}
 	err2 := decoder.Decode(&params)
 	if err2 != nil {
+		fmt.Println(err2)
 		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
 		return
 	}
@@ -190,12 +216,52 @@ func (db *DB) ValidationHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	chirpToSave := Chirp{
-		Body: washYourMouth(params.Body),
-		ID:   chirpCount + 1,
+		Body:      washYourMouth(params.Body),
+		ID:        chirpCount + 1,
+		Author_ID: userID,
 	}
-	db.NewDBEntry(chirpToSave, oldChirps)
+	db.NewDBEntry(chirpToSave, wholeDB)
 
 	respondWithJSON(w, 201, chirpToSave)
+}
+
+func (db *DB) DeleteHandler(w http.ResponseWriter, r *http.Request, cfg *apiConfig) {
+	userID, ok := db.AuthenticateUser(w, r, cfg)
+	if !ok {
+		return
+	}
+
+	wholeDB, err := db.LoadDB()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+
+	chirpIDStr := r.URL.Path[len("/api/chirps/"):]
+	chirpID, err1 := strconv.Atoi(chirpIDStr)
+	if err1 != nil {
+		respondWithError(w, http.StatusInternalServerError, "Not a valid chirp ID")
+		return
+	}
+
+	oldChirps := wholeDB.Chirps
+
+	targetChirp := oldChirps[chirpID]
+
+	if targetChirp.Author_ID != userID {
+		respondWithError(w, 403, "Only the author may delete a chirp")
+		return
+	}
+
+	deletedChirp := Chirp{
+		Author_ID: userID,
+		ID:        chirpID,
+		Body:      "Deleted",
+	}
+
+	oldChirps[chirpID] = deletedChirp
+
+	respondWithJSON(w, 200, "Chirp Deleted")
 }
 
 func (db *DB) UserHandler(w http.ResponseWriter, r *http.Request) {
@@ -230,19 +296,22 @@ func (db *DB) UserHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	userToSave := User{
-		Email:    params.Email,
-		ID:       userCount + 1,
-		Password: string(hashedPassword),
+		Email:         params.Email,
+		ID:            userCount + 1,
+		Password:      string(hashedPassword),
+		Is_Chirpy_Red: false,
 	}
 
 	type response struct {
-		Email string `json:"email"`
-		ID    int    `json:"id"`
+		Email         string `json:"email"`
+		ID            int    `json:"id"`
+		Is_Chirpy_Red bool   `json:"is_chirpy_red"`
 	}
 
 	newResponse := response{
-		Email: userToSave.Email,
-		ID:    userToSave.ID,
+		Email:         userToSave.Email,
+		ID:            userToSave.ID,
+		Is_Chirpy_Red: userToSave.Is_Chirpy_Red,
 	}
 
 	err4 := db.NewUser(userToSave)
@@ -256,9 +325,8 @@ func (db *DB) UserHandler(w http.ResponseWriter, r *http.Request) {
 
 func LoginHandler(w http.ResponseWriter, r *http.Request, cfg *apiConfig, db DB) {
 	type LoginAttempt struct {
-		Password           string `json:"password"`
-		Email              string `json:"email"`
-		Expires_in_seconds int    `json:"expires_in_seconds"`
+		Password string `json:"password"`
+		Email    string `json:"email"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -306,53 +374,112 @@ func LoginHandler(w http.ResponseWriter, r *http.Request, cfg *apiConfig, db DB)
 		return
 	}
 	type response struct {
-		ID    int    `json:"id"`
-		Email string `json:"email"`
-		Token string `json:"token"`
+		ID            int    `json:"id"`
+		Email         string `json:"email"`
+		Token         string `json:"token"`
+		Refresh_Token string `json:"refresh_token"`
+		Is_Chirpy_Red bool   `json:"is_chirpy_red"`
 	}
 
-	var expiry jwt.NumericDate
-	if params.Expires_in_seconds == 0 || params.Expires_in_seconds > 86400 {
-		expiry = *jwt.NewNumericDate(time.Now().Add(time.Hour * 24))
-	} else {
-		expiry = *jwt.NewNumericDate(time.Now().Add(time.Second * time.Duration(params.Expires_in_seconds)))
-	}
+	expiry := jwt.NewNumericDate(time.Now().Add(time.Hour))
+	refreshExpiry := jwt.NewNumericDate(time.Now().Add(time.Hour * 1440))
 
-	newClaim := jwt.RegisteredClaims{
-		Issuer:    "chirpy",
+	accessClaim := jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
 		IssuedAt:  jwt.NewNumericDate(time.Now()),
-		ExpiresAt: &expiry,
+		ExpiresAt: expiry,
+		Subject:   strconv.Itoa(currentUser.ID),
+	}
+	refreshClaim := jwt.RegisteredClaims{
+		Issuer:    "chirpy-refresh",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: refreshExpiry,
 		Subject:   strconv.Itoa(currentUser.ID),
 	}
 
-	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, newClaim)
-	signedToken, err3 := newToken.SignedString([]byte(cfg.secret))
+	newAccessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, accessClaim)
+	signedAccessToken, err3 := newAccessToken.SignedString([]byte(cfg.secret))
 	if err3 != nil {
 		fmt.Println(err3)
 		respondWithError(w, 500, "token signing failed")
 		return
 	}
+	newRefreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaim)
+	signedRefreshToken, err4 := newRefreshToken.SignedString([]byte(cfg.secret))
+	if err4 != nil {
+		fmt.Println(err4)
+		respondWithError(w, 500, "token signing failed")
+		return
+	}
+
+	dbToken := Auth{
+		Token:   signedRefreshToken,
+		Revoked: false,
+	}
+
+	wholeDB.Auths[signedRefreshToken] = dbToken
+	db.SaveDB(wholeDB)
 
 	userResponse := response{
-		ID:    currentUser.ID,
-		Email: currentUser.Email,
-		Token: signedToken,
+		ID:            currentUser.ID,
+		Email:         currentUser.Email,
+		Token:         signedAccessToken,
+		Refresh_Token: signedRefreshToken,
+		Is_Chirpy_Red: currentUser.Is_Chirpy_Red,
 	}
 
 	respondWithJSON(w, 200, userResponse)
 }
 
 func (db *DB) GetChirps(w http.ResponseWriter, r *http.Request) {
-	fullDB, err := db.LoadDB()
+	authID := r.URL.Query().Get("author_id")
+	sortOrder := r.URL.Query().Get("sort")
+
+	wholeDB, err := db.LoadDB()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Server encountered an error")
-	}
-	payload := []Chirp{}
-	for _, currentChirp := range fullDB.Chirps {
-		payload = append(payload, currentChirp)
+		return
 	}
 
-	respondWithJSON(w, 200, payload)
+	sortedKeys := make([]int, 0, len(wholeDB.Chirps))
+	for key := range wholeDB.Chirps {
+		sortedKeys = append(sortedKeys, key)
+	}
+	sort.Ints(sortedKeys)
+
+	payload := []Chirp{}
+
+	if authID != "" {
+		intID, err := strconv.Atoi(authID)
+		if err != nil {
+			respondWithError(w, 400, "Not a valid author ID")
+			return
+		}
+		for _, currentKey := range sortedKeys {
+			currentChirp := wholeDB.Chirps[currentKey]
+			if currentChirp.Author_ID == intID {
+				payload = append(payload, currentChirp)
+			}
+		}
+	} else {
+		for _, currentKey := range sortedKeys {
+			currentChirp := wholeDB.Chirps[currentKey]
+			payload = append(payload, currentChirp)
+		}
+	}
+
+	if sortOrder != "desc" {
+		respondWithJSON(w, 200, payload)
+		return
+	}
+
+	reversePayload := []Chirp{}
+
+	for i := len(payload) - 1; i >= 0; i-- {
+		reversePayload = append(reversePayload, payload[i])
+	}
+
+	respondWithJSON(w, 200, reversePayload)
 }
 
 func (db *DB) GetChirp(w http.ResponseWriter, r *http.Request) {
@@ -375,6 +502,34 @@ func (db *DB) GetChirp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, 200, payload)
+}
+
+func HeaderToToken(r *http.Request, cfg *apiConfig) (*jwt.Token, error) {
+	tokenHeader := r.Header.Get("Authorization")
+	tokenString := strings.Replace(tokenHeader, "Bearer ", "", 1)
+
+	type MyCustomClaims struct {
+		jwt.RegisteredClaims
+	}
+
+	localFunc := func(token *jwt.Token) (interface{}, error) {
+		return []byte(cfg.secret), nil
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, localFunc)
+	if err != nil {
+		fmt.Println(err)
+		errIntro := err.Error()[:18]
+		if errIntro == "token is malformed" {
+			return &jwt.Token{}, errors.New("Unauthorized")
+		}
+		if !token.Valid {
+			return &jwt.Token{}, errors.New("Unauthorized")
+		} else {
+			return &jwt.Token{}, err
+		}
+	}
+	return token, nil
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
@@ -437,6 +592,7 @@ func (db *DB) LoadDB() (dbStructure, error) {
 			saveFile, err2 := json.Marshal(dbStructure{
 				Chirps: map[int]Chirp{},
 				Users:  map[int]User{},
+				Auths:  map[string]Auth{},
 			})
 			if err2 != nil {
 				return dbStructure{}, nil
@@ -506,73 +662,74 @@ func (db *DB) NewUser(newUser User) error {
 	return nil
 }
 
-func (db *DB) AuthenticateUser(w http.ResponseWriter, r *http.Request, cfg *apiConfig) (User, bool) {
-	type UpdateAccount struct {
-		Password string `json:"password"`
-		Email    string `json:"email"`
-	}
+func (db *DB) AuthenticateUser(w http.ResponseWriter, r *http.Request, cfg *apiConfig) (int, bool) {
 
-	decoder := json.NewDecoder(r.Body)
-	params := UpdateAccount{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, 500, "Can't decode request")
-		return User{}, false
-	}
-
-	tokenHeader := r.Header.Get("Authorization")
-	tokenString := strings.Replace(tokenHeader, "Bearer ", "", 1)
-
-	type MyCustomClaims struct {
-		jwt.RegisteredClaims
-	}
-
-	localFunc := func(token *jwt.Token) (interface{}, error) {
-		return []byte(cfg.secret), nil
-	}
-
-	token, err2 := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, localFunc)
-	if err2 != nil {
-		fmt.Println(err2)
-		errIntro := err2.Error()[:18]
-		if errIntro == "token is malformed" {
+	token, err1 := HeaderToToken(r, cfg)
+	if err1 != nil {
+		fmt.Println(err1)
+		errIntro := err1.Error()
+		if errIntro == "Unauthorized" {
 			respondWithError(w, 401, "Unauthorized")
-			return User{}, false
+			return 0, false
 		}
-		if !token.Valid {
-			respondWithError(w, 401, "Unauthorized")
-		}
-		return User{}, false
-	}
-
-	id, err := token.Claims.GetSubject()
-	if err != nil {
 		respondWithError(w, 500, "Internal Server Error")
-		return User{}, false
+		return 0, false
 	}
 
-	intID, err3 := strconv.Atoi(id)
+	issuer, err2 := token.Claims.GetIssuer()
+	if err2 != nil {
+		respondWithError(w, 500, "Internal Server Error")
+		return 0, false
+	}
+	if issuer == "chirpy-refresh" {
+		respondWithError(w, 401, "Unauthorized")
+		return 0, false
+	}
+
+	userID, err3 := token.Claims.GetSubject()
 	if err3 != nil {
-		fmt.Println("failed conversion")
+		respondWithError(w, 500, "Internal Server Error")
+		return 0, false
 	}
 
-	authUser := User{
-		ID:       intID,
-		Email:    params.Email,
-		Password: params.Password,
+	intID, err4 := strconv.Atoi(userID)
+	if err4 != nil {
+		respondWithError(w, 500, "Internal Server Error")
+		return 0, false
 	}
 
-	return authUser, true
+	return intID, true
 }
 
 func (db *DB) UpdateUser(w http.ResponseWriter, r *http.Request, cfg *apiConfig) {
-	currentUser, ok := db.AuthenticateUser(w, r, cfg)
+	currentID, ok := db.AuthenticateUser(w, r, cfg)
 	if ok {
+		type UpdateAccount struct {
+			Password string `json:"password"`
+			Email    string `json:"email"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := UpdateAccount{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			respondWithError(w, 500, "Can't decode request")
+			return
+		}
+
 		wholeDB, err := db.LoadDB()
 		if err != nil {
 			fmt.Println("failed to load database")
 		}
 		allUsers := wholeDB.Users
+
+		//error handling if we somehow made it this far and the user isn't in the db?
+
+		currentUser := User{
+			Email:    params.Email,
+			Password: params.Password,
+			ID:       currentID,
+		}
 
 		hashedPassword, err2 := bcrypt.GenerateFromPassword([]byte(currentUser.Password), 0)
 		if err2 != nil {
@@ -589,15 +746,172 @@ func (db *DB) UpdateUser(w http.ResponseWriter, r *http.Request, cfg *apiConfig)
 		allUsers[currentUser.ID] = hashedUser
 		db.SaveDB(wholeDB)
 		type response struct {
-			Email string `json:"email"`
-			ID    int    `json:"id"`
+			Email         string `json:"email"`
+			ID            int    `json:"id"`
+			Is_Chirpy_Red bool   `json:"is_chirpy_red"`
 		}
 
 		myResponse := response{
-			Email: currentUser.Email,
-			ID:    currentUser.ID,
+			Email:         currentUser.Email,
+			ID:            currentUser.ID,
+			Is_Chirpy_Red: currentUser.Is_Chirpy_Red,
 		}
 		respondWithJSON(w, 200, myResponse)
 		return
 	}
+}
+
+func (db *DB) UpgradeUser(w http.ResponseWriter, r *http.Request, cfg *apiConfig) {
+	keyHeader := r.Header.Get("Authorization")
+	keyString := strings.Replace(keyHeader, "ApiKey ", "", 1)
+
+	if keyString != cfg.polkaKey {
+		respondWithError(w, 401, "API Key mismatch")
+		return
+	}
+
+	type userData struct {
+		User_ID int `json:"user_id"`
+	}
+	type UpgradeAccount struct {
+		Event string   `json:"event"`
+		Data  userData `json:"data"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := UpgradeAccount{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Can't decode request")
+		return
+	}
+	if params.Event != "user.upgraded" {
+		respondWithJSON(w, 200, []byte{})
+		return
+	}
+
+	wholeDB, err1 := db.LoadDB()
+	if err1 != nil {
+		respondWithError(w, 500, "Internal Server Error")
+		return
+	}
+	currentUser, exists := wholeDB.Users[params.Data.User_ID]
+	if !exists {
+		respondWithError(w, 404, "User not found")
+		return
+	}
+	currentUser.Is_Chirpy_Red = true
+
+	wholeDB.Users[params.Data.User_ID] = currentUser
+	db.SaveDB(wholeDB)
+
+	respondWithJSON(w, 200, []byte{})
+
+}
+
+func (db *DB) RefreshToken(w http.ResponseWriter, r *http.Request, cfg *apiConfig) {
+	//try printing token.raw to make sure it is what I think it is.
+	token, err := HeaderToToken(r, cfg)
+	if err != nil {
+		if err.Error() == "Unauthorized" {
+			respondWithError(w, 401, "Unathorized")
+			return
+		}
+		respondWithError(w, 500, "Internal Server Error")
+		return
+	}
+
+	issuer, err2 := token.Claims.GetIssuer()
+	if err2 != nil {
+		respondWithError(w, 500, "Internal Server Error")
+		return
+	}
+	if issuer == "chirpy-access" {
+		respondWithError(w, 401, "Unauthorized")
+		return
+	}
+
+	wholeDB, err3 := db.LoadDB()
+	if err3 != nil {
+		respondWithError(w, 500, "Internal Server Error")
+		return
+	}
+	refreshTokens := wholeDB.Auths
+
+	for _, dbToken := range refreshTokens {
+		if dbToken.Revoked && dbToken.Token == token.Raw {
+			respondWithError(w, 401, "Unauthorized")
+			return
+		}
+	}
+
+	type ResponseObject struct {
+		Token string `json:"token"`
+	}
+
+	expiry := jwt.NewNumericDate(time.Now().Add(time.Hour))
+
+	id, err4 := token.Claims.GetSubject()
+	if err4 != nil {
+		respondWithError(w, 500, "Internal Server Error")
+		return
+	}
+
+	accessClaim := jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: expiry,
+		Subject:   id,
+	}
+
+	newAccessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, accessClaim)
+	signedAccessToken, err5 := newAccessToken.SignedString([]byte(cfg.secret))
+	if err5 != nil {
+		fmt.Println(err5)
+		respondWithError(w, 500, "token signing failed")
+		return
+	}
+
+	response := ResponseObject{
+		Token: signedAccessToken,
+	}
+
+	respondWithJSON(w, 200, response)
+}
+
+func (db *DB) RevokeToken(w http.ResponseWriter, r *http.Request, cfg *apiConfig) {
+	token, err := HeaderToToken(r, cfg)
+	if err != nil {
+		if err.Error() == "Unauthorized" {
+			respondWithError(w, 401, "Unathorized")
+			return
+		}
+		respondWithError(w, 500, "Internal Server Error")
+		return
+	}
+
+	issuer, err2 := token.Claims.GetIssuer()
+	if err2 != nil {
+		respondWithError(w, 500, "Internal Server Error")
+		return
+	}
+	if issuer == "chirpy-access" {
+		respondWithError(w, 401, "Unauthorized")
+		return
+	}
+
+	revokedToken := Auth{
+		Token:       token.Raw,
+		Revoked:     true,
+		TimeRevoked: time.Now(),
+	}
+
+	wholeDB, err3 := db.LoadDB()
+	if err3 != nil {
+		respondWithError(w, 500, "Internal Server Error")
+	}
+	wholeDB.Auths[token.Raw] = revokedToken
+	db.SaveDB(wholeDB)
+
+	respondWithJSON(w, 200, []byte{})
 }
